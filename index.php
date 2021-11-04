92366b002857de4d00227096cccce48ef204c20f
etiennezastko <etiennezastko@hotmail.fr>
2021-11-04 14:17:50 +0100

    1 container en place

--- index.php
+++ index.php
@@ -24,21 +24,45 @@
   </a>
 </div>
 
-
-
-
-
 <div class="container-title">
-            <h1 class="title_1">Beautiful food & takeaway, delivered to your door</h1></div>
+            <h1 class="title_1">Beautiful food & takeaway, <span>delivered</span> to your door</h1></div>
 
 <div class="content">
 <div>
-<img class="image-1 " id="image-i" src="./assets/images/hero_image.png" alt=""></div>
-        <div class="container-texte">
+    <img class="image-1 "src="./assets/images/hero_image.png" alt=""></div>
+    <div class="container-texte">
             <p class="texte">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.</p>
-            
-        <button class="button-color">Place an Order</button>
-        </div>
+            <button class="button-color">Place an Order</button>
+    </div>
+    
+    </div>
+
+
+<div class="content-2">
+<div>
+    <img class="image-2 "src="./assets/images/eat.png "alt="image"></div>
+    <div class="container-texte">
+            <p class="texte">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.</p>
+            <button class="button-color">Place an Order</button>
+    </div>
+    </div>
+<div class="content-3">
+<div>
+    <img class="image-2 "src="./assets/images/ "alt="image"></div>
+    <div class="container-texte">
+            <p class="texte">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.</p>
+            <button class="button-color">Place an Order</button>
+    </div>
+    </div>
+   
+
+
+
+
+
+
+
+   
 
 
 
@@ -47,36 +71,6 @@
 
 
 
-
-
-        <h1 class="title_1">The home of fresh products</h1>
-
-
-        <div class="container-texte-2">
-            <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.
-            </p>
-        </div>
-
-        <div class="container-texte-2">
-
-            <p> </p>
-            <div>
-            <img class="image-1 " id="image-i" src="./assets/images/eat.png" alt="">
-            </div>
-        </div>
-
-        <div class="container-texte-3">
-
-            <p>
-             
-            </p>
-            <img class="image-1" src="../assets/images/brainstorming-rafiki.svg" alt="">
-        </div>
-
-        <div class="container-title-2">
-            <h1 class="title_2">Qui est touché par ce sujet ?</h1>
-        </div>
 
 <script src="main.js"></script>
 </body>
