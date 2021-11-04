6986ddf6dfe0a1ad6aa292727848fa8942bb7884
etiennezastko <etiennezastko@hotmail.fr>
2021-11-04 11:55:44 +0100

    first-commit

--- index.php
+++ index.php
@@ -5,9 +5,78 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Document</title>
 </head>
 <body>
-    
+<img src="./assets/images/logo.png" class ="logo"alt="">  
+
+
+<div class="topnav" id="myTopnav">
+
+  <a href="#News">Home</a>
+  <a href="#contact">Order</a>
+  <a href="#about">Company</a>
+  <a href="#about">FAQ</a>
+  <a href="#about">Contact</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
+
+
+
+
+
+<div class="container-title">
+            <h1 class="title_1">Beautiful food & takeaway, delivered to your door</h1></div>
+<div class="container-full">
+<img class="image-1 " id="image-i" src="./assets/images/hero_image.png" alt="">
+        <div class="container-texte">
+            <p class="texte">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.</p>
+        </div>
+
+        <button class="button-color">Place an Order</button>
+        <h1 class="title_1">The home of fresh products</h1>
+
+
+        <div class="container-texte-2">
+            <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.
+            </p>
+
+     
+        </div>
+
+        <div class="container-texte-2">
+
+            <p> </p>
+            <div>
+            <img class="image-1 " id="image-i" src="./assets/images/eat.png" alt="">
+            </div>
+        </div>
+
+        <div class="container-texte-3">
+
+            <p>
+             
+            </p>
+
+            <img class="image-1" src="../assets/images/brainstorming-rafiki.svg" alt="">
+        </div>
+
+        <div class="container-title-2">
+            <h1 class="title_2">Qui est touché par ce sujet ?</h1>
+        </div>
+
+
+
+        
+      
+
+
+
+
+            <script src="main.js"></script>
 </body>
 </html>
